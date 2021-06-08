7ccd80934a95cb402b55a9514ab98ba817b22ccb
Logan Demmy <frank.demmy@gmail.com>
2021-06-08 13:56:45 -0500

    created classes of crew and bank

---
+++ Hacker.cs
@@ -0,0 +1,9 @@
+namespace heistpart2
+{
+    public class Hacker : IRobber
+    {
+        public string Name { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public int SkillLevel { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public int PercentageCut { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+    }
+}
